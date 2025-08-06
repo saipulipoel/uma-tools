8743e785fec7adf73a745f3b9ba211d742746f66
pecan <turkchess123@gmail.com>
2025-08-06 05:44:47 -0600

    default master.mdb location in update.bat

--- umalator-global/update.bat
+++ umalator-global/update.bat
@@ -1,6 +1,13 @@
 @echo off
-perl make_global_skill_data.pl master.mdb > skill_data.json
-perl make_global_skillnames.pl master.mdb > skillnames.json
-perl make_global_skill_meta.pl master.mdb > skill_meta.json
-perl make_global_uma_info.pl master.mdb
+
+if "%1" == "" (
+  set mastermdb=%APPDATA%\..\LocalLow\Cygames\umamusume\master\master.mdb
+) else (
+  set mastermdb=%1
+)
+
+perl make_global_skill_data.pl %mastermdb% > skill_data.json
+perl make_global_skillnames.pl %mastermdb% > skillnames.json
+perl make_global_skill_meta.pl %mastermdb% > skill_meta.json
+perl make_global_uma_info.pl %mastermdb%
 node build.mjs
